e9ed51ed165ae0ae601cd40755fffbce2bf51a34
Kangbok Lee <gangbok119@gmail.com>
2017-09-25 14:33:17 +0900

    Post model 정의, migrations 생성, admin 추가

--- djangogirls/blog/admin.py
+++ djangogirls/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Post
+# from blog.models import Post
 # Register your models here.
+
+admin.site.register(Post) #장고에서 제공하는 관리자 기능
+# 이 테이블을 관리자 페이지에서 관리할 수 있다.
--- djangogirls/blog/models.py
+++ djangogirls/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+
+class Post(models.Model):
+    # settings.AUTH_USER_MODEL > auth.User
+    author = models.ForeignKey(settings.AUTH_USER_MODEL) #작성자에 대한 연결 - 다(포스트)대일(유저)
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)
