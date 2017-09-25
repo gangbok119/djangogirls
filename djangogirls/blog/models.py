1cfed10ba2aa0b7af5d5c6e9f183fa604de11f49
Kangbok Lee <gangbok119@gmail.com>
2017-09-25 15:21:05 +0900

    blog __str__(): - 포스트의 제목을 보이도록 하는 view 작성

--- djangogirls/blog/models.py
+++ djangogirls/blog/models.py
@@ -6,8 +6,12 @@ from django.conf import settings
 
 class Post(models.Model):
     # settings.AUTH_USER_MODEL > auth.User
-    author = models.ForeignKey(settings.AUTH_USER_MODEL) #작성자에 대한 연결 - 다(포스트)대일(유저)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)  # 작성자에 대한 연결 - 다(포스트)대일(유저)
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.title
--- djangogirls/config/settings.py
+++ djangogirls/config/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -27,21 +26,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',  #기본 어플리케이션
+    'django.contrib.admin',  # 기본 어플리케이션
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #서드파티 어플리케이션
-    'django_extensions',
+    # 서드파티 어플리케이션
+    'django_extensions',  # 콤마를 꼭 넣자...
 
-    # 유저 어플리케이
+    # 유저 어플리케이션
     'blog',
 ]
 
@@ -75,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -85,7 +82,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -118,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
