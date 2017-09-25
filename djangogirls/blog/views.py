87c97dafd82de1cfbda84acc7d1b66bf80eeb6a3
Kangbok Lee <gangbok119@gmail.com>
2017-09-25 16:24:50 +0900

    post_list view에서 template, context 변수 사용, urls와 연결

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,8 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Post
+
 
 # Create your views here.
 
-def post_list(request): #view는 무조건 하나의 인수를 받는다.
-    return HttpResponse('')
 
+def post_list(request):  # view는 무조건 하나의 인수를 받는다.
+    posts = Post.objects.all()
+    context = {
+        # posts key의 value는 QuerySet
+        'posts':posts,
+    }
+    #rendering
+    return render(request, 'blog/post_list.html', context)
