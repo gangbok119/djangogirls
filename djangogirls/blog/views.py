15857905f767c2ffc2a646a7f77dfd122f4964fa
Kangbok Lee <gangbok119@gmail.com>
2017-09-25 15:54:49 +0900

    TEMPLATE_DIR 설정

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request): #view는 무조건 하나의 인수를 받는다.
+    return HttpResponse('')
+
